d3fa16bea7daafad01c17ce854ac8ea30fd04dc7
Maxime Moreillon <m.moreillon@gmail.com>
2022-06-02 08:02:38 +0000

    prometheus metrics

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
-const bodyParser = require('body-parser')
+const apiMetrics = require('prometheus-api-metrics')
 const {version, author} = require('./package.json')
 const db = require('./db.js')
 const mail = require('./mail.js')
@@ -10,13 +10,19 @@ const users_router = require('./routes/users.js')
 
 dotenv.config()
 
-const EXPRESS_PORT = process.env.EXPRESS_PORT || 80
+const {
+  EXPRESS_PORT = 80,
+  ALLOW_REGISTRATION
+} = process.env
+
 
 db.connect()
 
 const app = express()
-app.use(bodyParser.json())
+
+app.use(express.json())
 app.use(cors())
+app.use(apiMetrics())
 
 app.get('/', (req, res) => {
   res.send({
@@ -28,7 +34,7 @@ app.get('/', (req, res) => {
       db: db.db,
       connected: db.connected(),
     },
-    registration_allowed: process.env.ALLOW_REGISTRATION || false,
+    registration_allowed: ALLOW_REGISTRATION || false,
     smtp: {
       host: mail.options.host || 'undefined',
       port: mail.options.port || 'undefined',
