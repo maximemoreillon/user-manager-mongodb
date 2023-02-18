fc76eb7f24202be9f706577b6c4ab734c99bfa00
Maxime Moreillon <m.moreillon@gmail.com>
2023-02-19 08:58:29 +0900

    last_login registration

--- models/user.js
+++ models/user.js
@@ -1,19 +1,21 @@
-const {Schema, model} = require('mongoose')
+const { Schema, model } = require("mongoose")
 
 const userSchema = new Schema({
-  username: {type: String, unique: true, trim: true, required: true},
-  email_address: {type: String, unique: true, trim: true, sparse: true},
+  username: { type: String, unique: true, trim: true, required: true },
+  email_address: { type: String, unique: true, trim: true, sparse: true },
   password_hashed: { type: String, required: true, select: false },
   display_name: String,
   avatar_src: String,
   isAdmin: Boolean,
-  administrator: Boolean, // LEGACY
   last_login: Date,
   creation_date: Date,
-  activated: {type: Boolean, default: false},
-  locked: {type: Boolean, default: false},
+  activated: { type: Boolean, default: false },
+  locked: { type: Boolean, default: false },
+
+  // Legacy
+  administrator: Boolean,
 })
 
-const User = model('User', userSchema)
+const User = model("User", userSchema)
 
 module.exports = User
