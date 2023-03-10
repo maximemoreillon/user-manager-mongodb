aa4383ad0082124f822e5ce366d73489eadae8e8
Maxime Moreillon <m.moreillon@gmail.com>
2023-03-11 06:57:56 +0900

    async express errors

--- controllers/users.js
+++ controllers/users.js
@@ -1,268 +1,206 @@
-const createHttpError = require('http-errors')
-const dotenv = require('dotenv')
-const User = require('../models/user.js')
-const { hash_password } = require('../auth.js')
-const { passwordUpdateSchema } = require('../schemas/password.js')
+const createHttpError = require("http-errors")
+const dotenv = require("dotenv")
+const User = require("../models/user.js")
+const { hash_password } = require("../auth.js")
+const { passwordUpdateSchema } = require("../schemas/password.js")
 const {
   send_activation_email,
   send_password_reset_email,
-} = require('../mail.js')
+} = require("../mail.js")
 const {
   newUserSchema,
   userUpdateSchema,
-  userAdminUpdateSchema
-} = require('../schemas/user.js')
+  userAdminUpdateSchema,
+} = require("../schemas/user.js")
 
 dotenv.config()
 
-
-exports.create_user = async (req, res, next) => {
+exports.create_user = async (req, res) => {
+  const properties = req.body
 
   try {
-    // Todo: validation with joy
-    const properties = req.body
-    // const { username, password, email_address } = req.body
-
-    try {
-      await newUserSchema.validateAsync(properties)
-    }
-    catch (error) {
-      throw createHttpError(400, error)
-    }
-
-    const { username, password, email_address } = properties
-
-    const current_user = res.locals.user
-    const current_user_is_admin = current_user?.isAdmin
-
-    // Email activation only necessary if user registers himself
-    const activated = current_user_is_admin ? true : false
-
-    if(!process.env.ALLOW_REGISTRATION && !current_user_is_admin) {
-      throw createHttpError(403, `Registration is not allowed unless administrator`)
-    }
-
-    const password_hashed = await hash_password(password)
-
-    const user = await User.create({
-      username,
-      password_hashed,
-      email_address,
-      display_name: username,
-      creation_date: new Date(),
-      activated,
-    })
-
-    // Send activation email if necessary
-    const mail_options = {url: req.headers.origin, user}
-    if(!activated) await send_activation_email(mail_options)
-
-    res.send(user)
-    console.log(`[Mongoose] User ${user._id} created`)
-
-
-
-  }
-  catch (error) {
-    next(error)
+    await newUserSchema.validateAsync(properties)
+  } catch (error) {
+    throw createHttpError(400, error)
   }
 
+  const { username, password, email_address } = properties
+
+  const current_user = res.locals.user
+  const current_user_is_admin = current_user?.isAdmin
+
+  // Email activation only necessary if user registers himself
+  const activated = current_user_is_admin ? true : false
+
+  if (!process.env.ALLOW_REGISTRATION && !current_user_is_admin) {
+    throw createHttpError(
+      403,
+      `Registration is not allowed unless administrator`
+    )
+  }
+
+  const password_hashed = await hash_password(password)
+
+  const user = await User.create({
+    username,
+    password_hashed,
+    email_address,
+    display_name: username,
+    creation_date: new Date(),
+    activated,
+  })
+
+  // Send activation email if necessary
+  const mail_options = { url: req.headers.origin, user }
+  if (!activated) await send_activation_email(mail_options)
+
+  res.send(user)
+  console.log(`[Mongoose] User ${user._id} created`)
 }
 
-exports.delete_user = async (req, res, next) => {
+exports.delete_user = async (req, res) => {
+  const { user } = res.locals
+  let { user_id } = req.params
 
-  try {
-    const {user} = res.locals
-    let {user_id} = req.params
+  if (user_id === "self") user_id = user._id
+  if (!user_id) throw createHttpError(400, `User ID not defined`)
 
-    if(user_id === 'self') user_id = user._id
-    if (!user_id) throw createHttpError(400, `User ID not defined`)
-
-    if(user._id.toString() !== user_id.toString() && !user.isAdmin) {
-      throw createHttpError(403, `Not allowed to delete another user`)
-    }
-
-    await User.deleteOne({_id: user_id})
-
-    res.send({_id: user_id})
-
-    console.log(`[Mongoose] User ${user_id} deleted`)
-
-  }  
-  catch (error) {
-    next(error)
+  if (user._id.toString() !== user_id.toString() && !user.isAdmin) {
+    throw createHttpError(403, `Not allowed to delete another user`)
   }
+
+  await User.deleteOne({ _id: user_id })
+
+  res.send({ _id: user_id })
+
+  console.log(`[Mongoose] User ${user_id} deleted`)
 }
 
-exports.update_user = async (req, res, next) => {
+exports.update_user = async (req, res) => {
+  const { user } = res.locals
+  let { user_id } = req.params
+  const properties = req.body
+
+  if (user_id === "self") user_id = user._id
+  if (!user_id) throw createHttpError(400, `User ID not defined`)
+
+  if (user._id.toString() !== user_id.toString() && !user.isAdmin) {
+    throw createHttpError(403, `Not allowed to update another user`)
+  }
 
   try {
-
-    const {user} = res.locals
-    let {user_id} = req.params
-    const properties = req.body
-
-    if(user_id === 'self') user_id = user._id
-    if (!user_id) throw createHttpError(400, `User ID not defined`)
-
-    if(user._id.toString() !== user_id.toString() && !user.isAdmin) {
-      throw createHttpError(403, `Not allowed to update another user`)
-    }
-
-    try {
-      if (user.isAdmin) await userAdminUpdateSchema.validateAsync(properties)
-      else await userUpdateSchema.validateAsync(properties)
-    }
-    catch (error) {
-      throw createHttpError(403, error)
-    }
-
-    const result = await User.updateOne({_id: user_id}, properties)
-
-    res.send(result)
-    console.log(`[Mongoose] User ${user_id} updated`)
-
-  }  
-  catch (error) {
-    next(error)
+    if (user.isAdmin) await userAdminUpdateSchema.validateAsync(properties)
+    else await userUpdateSchema.validateAsync(properties)
+  } catch (error) {
+    throw createHttpError(403, error)
   }
+
+  const result = await User.updateOne({ _id: user_id }, properties)
+
+  console.log(`[Mongoose] User ${user_id} updated`)
+  res.send(result)
 }
 
-exports.get_user = async (req, res, next) => {
+exports.get_user = async (req, res) => {
+  // Get user ID from query
+  let { user_id } = req.params
 
-  try {
-    // Get user ID from query
-    let {user_id} = req.params
+  // If user is self, then use ID of user currently logged in
+  if (user_id === "self") user_id = res.locals.user
+  if (!user_id) throw createHttpError(400, `User ID not defined`)
 
-    // If user is self, then use ID of user currently logged in
-    if(user_id === 'self') user_id = res.locals.user
-    if (!user_id) throw createHttpError(400, `User ID not defined`)
-
-    const user = await User.findById(user_id)
-    res.send(user)
-    console.log(`[Mongoose] User ${user._id} queried`)
-  }
-  catch (error) {
-    next(error)
-  }
-
+  const user = await User.findById(user_id)
+  res.send(user)
+  console.log(`[Mongoose] User ${user._id} queried`)
 }
 
-exports.update_password = async (req, res, next) => {
-
+exports.update_password = async (req, res) => {
   try {
-
-    try {
-      await passwordUpdateSchema.validateAsync(req.body)
-    }
-    catch (error) {
-      throw createHttpError(400, error.message)
-    }
-
-    const { new_password, new_password_confirm } = req.body
-
-
-    const current_user = res.locals.user
-
-    let user_id = req.params.user_id
-    if(user_id === 'self') user_id = current_user._id
-
-    if(String(user_id) !== String(current_user._id) && !current_user.isAdmin) {
-      throw createHttpError(403, `Unauthorized to modify another user's password`)
-    }
-
-    const password_hashed = await hash_password(new_password)
-    const result = await User.updateOne({_id: user_id}, {password_hashed})
-
-    console.log(`[Mongoose] Password of user ${user_id} updated`)
-    res.send(result)
+    await passwordUpdateSchema.validateAsync(req.body)
+  } catch (error) {
+    throw createHttpError(400, error.message)
   }
 
-  catch (error) {
-    next(error)
+  const { new_password, new_password_confirm } = req.body
+  // TODO: check with password confirm
+
+  const current_user = res.locals.user
+
+  let user_id = req.params.user_id
+  if (user_id === "self") user_id = current_user._id
+
+  if (String(user_id) !== String(current_user._id) && !current_user.isAdmin) {
+    throw createHttpError(403, `Unauthorized to modify another user's password`)
   }
 
+  const password_hashed = await hash_password(new_password)
+  const result = await User.updateOne({ _id: user_id }, { password_hashed })
 
-
+  console.log(`[Mongoose] Password of user ${user_id} updated`)
+  res.send(result)
 }
 
-exports.get_users = async (req, res, next) => {
+exports.get_users = async (req, res) => {
+  // TODO: filters
+  const {
+    skip = 0,
+    limit = 50,
+    sort = "_id",
+    order = 1,
+    ids,
+    search,
+  } = req.query
 
-  try {
-    // TODO: filters
-    const {
-      skip = 0,
-      limit = 50,
-      sort = '_id',
-      order = 1,
-      ids,
-      search,
-    } = req.query
+  const sort_and_order = {}
+  sort_and_order[sort] = order
 
-    const sort_and_order = {}
-    sort_and_order[sort] = order
-
-    let query = {}
-    if (search) {
-      const searched_fields = ['username', 'email_address', 'display_name']
-      query['$or'] = searched_fields.map((item) => ({ [item]: { $regex: search, $options: 'i' } }))
-    }
-
-    // A list of user IDs can be passed as filter
-    if (ids) query['$or'] = ids.map(_id => ({_id}) )
-    
-
-    const count = await User.countDocuments(query)
-
-    const users = await User
-      .find(query)
-      .skip(Number(skip))
-      .sort(sort_and_order)
-      .limit(Number(limit))
-
-    console.log(`[Mongoose] Users queried`)
-
-    res.send({users, count})
-
-  }
-  catch (error) {
-    next(error)
+  let query = {}
+  if (search) {
+    const searched_fields = ["username", "email_address", "display_name"]
+    query["$or"] = searched_fields.map((item) => ({
+      [item]: { $regex: search, $options: "i" },
+    }))
   }
 
+  // A list of user IDs can be passed as filter
+  if (ids) query["$or"] = ids.map((_id) => ({ _id }))
+
+  const count = await User.countDocuments(query)
+
+  const users = await User.find(query)
+    .skip(Number(skip))
+    .sort(sort_and_order)
+    .limit(Number(limit))
+
+  console.log(`[Mongoose] Users queried`)
+
+  res.send({ users, count })
 }
 
-exports.password_reset = async (req, res, next) => {
+exports.password_reset = async (req, res) => {
+  const { email_address } = req.body
+  if (!email_address) throw createHttpError(400, `Missing email_address`)
+  const user = await User.findOne({ email_address })
+  if (!user)
+    throw createHttpError(
+      404,
+      `User with email_address ${email_address} not found`
+    )
 
-  try {
-    const { email_address } = req.body
-    if (!email_address) throw createHttpError(400, `Missing email_address`)
-    const user = await User.findOne({ email_address })
-    if (!user) throw createHttpError(404, `User with email_address ${email_address} not found`)
+  const mail_options = { url: req.headers.origin, user }
+  await send_password_reset_email(mail_options)
 
-    const mail_options = { url: req.headers.origin, user }
-    await send_password_reset_email(mail_options)
-
-    res.send({ email_address })
-
-  }
-  catch (error) {
-    next(error)
-  }
-
-
+  res.send({ email_address })
 }
 
 exports.create_admin_account = async () => {
+  const {
+    ADMIN_USERNAME: admin_username = "admin",
+    ADMIN_PASSWORD: admin_password = "admin",
+  } = process.env
 
+  const password_hashed = await hash_password(admin_password)
   try {
-
-    const {
-      ADMIN_USERNAME: admin_username = 'admin',
-      ADMIN_PASSWORD: admin_password = 'admin'
-    } = process.env
-
-    const password_hashed = await hash_password(admin_password)
-
     const admin = await User.create({
       username: admin_username,
       display_name: admin_username,
@@ -273,32 +211,24 @@ exports.create_admin_account = async () => {
     })
 
     console.log(`[Mongoose] Admin account created`)
-
-  } 
-  catch (error) {
-    if(error.code === 11000) console.log(`[Mongoose] Admin account already exists`)
+  } catch (error) {
+    if (error.code === 11000)
+      console.log(`[Mongoose] Admin account already exists`)
     else console.log(error)
   }
-
 }
-
 
 exports.rename_admin_property = async () => {
   // Used to rename field 'administrator' into 'isAdmin'
   // For compatibility with Neo4J version
 
+  const actions = { $rename: { administrator: "isAdmin" } }
+  const options = { multi: true }
+
   try {
-
-
-    const actions = { $rename: { administrator: 'isAdmin' } }
-    const options = { multi: true }
-
     await User.updateMany({}, actions, options)
-
     console.log(`[Mongoose] administrator property renamed`)
-
   } catch (error) {
     console.log(error)
   }
-
 }
--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-const express = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
-const apiMetrics = require('prometheus-api-metrics')
-const {version, author} = require('./package.json')
-const db = require('./db.js')
-const mail = require('./mail.js')
-const auth_router = require('./routes/auth.js')
-const users_router = require('./routes/users.js')
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+const apiMetrics = require("prometheus-api-metrics")
+const { version, author } = require("./package.json")
+const db = require("./db.js")
+const mail = require("./mail.js")
+const auth_router = require("./routes/auth.js")
+const users_router = require("./routes/users.js")
+require("express-async-errors")
 
 dotenv.config()
 
-const {
-  EXPRESS_PORT = 80,
-  ALLOW_REGISTRATION
-} = process.env
-
+const { EXPRESS_PORT = 80, ALLOW_REGISTRATION } = process.env
 
 db.connect()
 
@@ -24,9 +21,9 @@ app.use(express.json())
 app.use(cors())
 app.use(apiMetrics())
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send({
-    application_name: 'User manager (Mongoose version)',
+    application_name: "User manager (Mongoose version)",
     version,
     author,
     mongodb: {
@@ -36,15 +33,15 @@ app.get('/', (req, res) => {
     },
     registration_allowed: ALLOW_REGISTRATION || false,
     smtp: {
-      host: mail.options.host || 'undefined',
-      port: mail.options.port || 'undefined',
-      from: mail.options.from || 'undefined',
-    }
+      host: mail.options.host || "undefined",
+      port: mail.options.port || "undefined",
+      from: mail.options.from || "undefined",
+    },
   })
 })
 
-app.use('/auth', auth_router)
-app.use('/users', users_router)
+app.use("/auth", auth_router)
+app.use("/users", users_router)
 
 app.listen(EXPRESS_PORT, () => {
   console.log(`[Express] App listening on ${EXPRESS_PORT}`)
@@ -58,12 +55,11 @@ app.use((error, req, res, next) => {
   res.status(statusCode).send(message)
 })
 
-
 // exporting app for tests
 exports.app = app
 
 // Stop on CTRL C (for docker)
-process.on('SIGINT', () => {
-  console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" )
-  process.exit(0);
+process.on("SIGINT", () => {
+  console.log("\nGracefully shutting down from SIGINT (Ctrl-C)")
+  process.exit(0)
 })
