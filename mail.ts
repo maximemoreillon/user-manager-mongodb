dc78146d2f010bbecfe29ce5ac751498ba88f6ef
Maxime Moreillon <m.moreillon@gmail.com>
2023-06-21 08:16:08 +0900

    email minor

--- mail.ts
+++ mail.ts
@@ -20,28 +20,19 @@ export const options = {
 
 export const transporter = nodemailer.createTransport(options as any)
 
-const send_email = (email: any) =>
-  new Promise((resolve, reject) => {
-    // This might already be a promise
-    transporter.sendMail(email, (error: any, info: any) => {
-      if (error) reject(error)
-      else resolve(info)
-    })
-  })
-
 export const send_activation_email = async ({ url, user }: any) => {
   try {
     const { email_address } = user
     const token = await generate_token(user)
 
-    const activation_email = {
+    const email = {
       from: SMTP_FROM,
       to: email_address,
       subject: "Account activation",
       text: `Click the following link to register your account: ${url}/activate?token=${token}`,
     }
 
-    await send_email(activation_email)
+    await transporter.sendMail(email)
 
     console.log(`[Mail] Sent activation email to user ${email_address}`)
   } catch (e) {
@@ -54,14 +45,14 @@ export const send_password_reset_email = async ({ url, user }: any) => {
     const { email_address } = user
     const token = await generate_token(user)
 
-    const activation_email = {
+    const email = {
       from: SMTP_FROM,
       to: email_address,
       subject: "Password reset",
       text: `Click the following link to reset your password: ${url}/password_update?token=${token}`,
     }
 
-    await send_email(activation_email)
+    await transporter.sendMail(email)
 
     console.log(`[Mail] Sent password reset email to user ${email_address}`)
   } catch (e) {
