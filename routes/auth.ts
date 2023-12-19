3b5c9c4881a3db9e577b1408bca952135937142b
Maxime Moreillon <m.moreillon@gmail.com>
2023-12-20 08:28:19 +0900

    token route

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,8 +1,11 @@
 import express from "express"
 import { login } from "../auth"
+import { decodeToken } from "../controllers/tokens"
 
 const router = express.Router()
 
 router.route("/login").post(login)
+
+router.route("/token").post(decodeToken)
 
 export default router
